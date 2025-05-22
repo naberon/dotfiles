c14d1a5607e28b1e6fec31689edaa45d4f7e713b
naberon <naberon86@gmail.com>
2025-05-22 13:11:50 +0900

    add wezterm conf

--- mklink.bat
+++ mklink.bat
@@ -1,4 +1,5 @@
 mklink %USERPROFILE%\_vimrc %USERPROFILE%\dotfiles\vimrc
 mklink %USERPROFILE%\_gvimrc %USERPROFILE%\dotfiles\gvimrc
 mklink /D %USERPROFILE%\vimfiles %USERPROFILE%\dotfiles\vim
+mklink %USERPROFILE%\.wezterm.lua %USERPROFILE%\workspace\dotfiles\wezterm.lua
 echo "hoge"
