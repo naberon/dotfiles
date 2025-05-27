9ee2f478d2d2d6753b495819740f2a13a2ba8750
naberon <naberon86@gmail.com>
2025-05-27 17:54:41 +0900

    neovim 入力補完修正

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -62,6 +62,8 @@ return {
         --  Most Language Servers support renaming across files, etc.
         map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
 
+        map('gk', vim.lsp.buf.hover, '[R]e[n]ame')
+
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
         map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
