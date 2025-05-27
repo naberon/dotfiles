5b5dfb7e5944af287624de9667060b3c476d3c7a
naberon <naberon86@gmail.com>
2025-05-27 17:52:49 +0900

    neovimの背景色を透過に変更

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -4,8 +4,11 @@ return {
   config = function()
     ---@diagnostic disable-next-line: missing-fields
     require('tokyonight').setup {
+      transparent = true,
       styles = {
         comments = { italic = false }, -- Disable italics in comments
+        -- sidebars = 'transparent',
+        -- floats = 'transparent',
       },
     }
 
