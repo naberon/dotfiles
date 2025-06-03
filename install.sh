5bb562ba6f54a74dd4f2d6b9685d781b97696c5b
naberon <naberon86@gmail.com>
2025-06-03 21:39:13 +0900

    add install script

--- install.sh
+++ install.sh
@@ -2,13 +2,17 @@
 
 echo "Deleting the old files"
 echo "Symlinking files"
-ln -s ~/dotfiles/gemrc ~/.gemrc
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/tigrc ~/.tigrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/zsh ~/.zsh
-ln -s ~/dotfiles/zshenv ~/.zshenv
-ln -s ~/dotfiles/zshprofile ~/.zshprofile
+#ln -s /gemrc ~/.gemrc
+#ln -s ~/dotfiles/gitconfig ~/.gitconfig
+#ln -s ~/dotfiles/tigrc ~/.tigrc
+#ln -s ~/dotfiles/zshrc ~/.zshrc
+#ln -s ~/dotfiles/zsh ~/.zsh
+
+ln -s ~/workspace/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/workspace/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/workspace/dotfiles/tigrc ~/.tigrc
+
+ln -s ~/workspace/dotfiles/tmux ~/.config/tmux
+ln -s ~/workspace/dotfiles/sheldon ~/.config/sheldon
+ln -s ~/workspace/dotfiles/wezterm ~/.config/wezterm
+ln -s ~/workspace/dotfiles/nvim ~/.config/nvim
---
+++ unlink.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Deleting the old files"
+echo "Symlinking files"
+unlink ~/.zshrc
+#ln -s ~/dotfiles/gemrc ~/.gemrc
+#ln -s ~/dotfiles/gitconfig ~/.gitconfig
+#ln -s ~/dotfiles/tigrc ~/.tigrc
+#ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+#ln -s ~/dotfiles/vimrc ~/.vimrc
+#ln -s ~/dotfiles/vim ~/.vim
+#ln -s ~/dotfiles/zshrc ~/.zshrc
+#ln -s ~/dotfiles/zsh ~/.zsh
+#ln -s ~/dotfiles/zshenv ~/.zshenv
+#ln -s ~/dotfiles/zshprofile ~/.zshprofile
