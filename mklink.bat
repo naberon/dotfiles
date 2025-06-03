db11795f8211e383e1305f72043efb7d83b05bd2
naberon <naberon86@gmail.com>
2025-06-03 18:43:04 +0900

    move wezterm.lua

--- mklink.bat
+++ mklink.bat
@@ -1,5 +1,1 @@
-mklink %USERPROFILE%\_vimrc %USERPROFILE%\dotfiles\vimrc
-mklink %USERPROFILE%\_gvimrc %USERPROFILE%\dotfiles\gvimrc
-mklink /D %USERPROFILE%\vimfiles %USERPROFILE%\dotfiles\vim
-mklink %USERPROFILE%\.wezterm.lua %USERPROFILE%\workspace\dotfiles\wezterm.lua
-echo "hoge"
+mklink %USERPROFILE%\.wezterm.lua %USERPROFILE%\workspace\dotfiles\wezterm\wezterm.lua
