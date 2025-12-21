92c251c16f342efef5d608a94dde1807a3130487
naberon <naberon86@gmail.com>
2025-12-21 19:26:12 +0900

    インストールスクリプトの修正

--- install.sh
+++ install.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
-echo "Deleting the old files"
 echo "Symlinking files"
-#ln -s /gemrc ~/.gemrc
-#ln -s ~/dotfiles/gitconfig ~/.gitconfig
-#ln -s ~/dotfiles/tigrc ~/.tigrc
-#ln -s ~/dotfiles/zshrc ~/.zshrc
-#ln -s ~/dotfiles/zsh ~/.zsh
 
-ln -s ~/workspace/dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/workspace/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/workspace/dotfiles/tigrc ~/.tigrc
+# シンボリックリンクを作成する関数（既存の場合はスキップ）
+create_link() {
+    local src=$1
+    local dest=$2
+    if [ -L "$dest" ]; then
+        echo "Skip: $dest (already exists)"
+    else
+        ln -s "$src" "$dest"
+        echo "Created: $dest -> $src"
+    fi
+}
 
-ln -s ~/workspace/dotfiles/tmux ~/.config/tmux
-ln -s ~/workspace/dotfiles/sheldon ~/.config/sheldon
-ln -s ~/workspace/dotfiles/wezterm ~/.config/wezterm
-ln -s ~/workspace/dotfiles/nvim ~/.config/nvim
+create_link ~/workspace/dotfiles/zsh/.zshrc ~/.zshrc
+create_link ~/workspace/dotfiles/gitconfig ~/.gitconfig
+create_link ~/workspace/dotfiles/tigrc ~/.tigrc
+
+create_link ~/workspace/dotfiles/tmux ~/.config/tmux
+create_link ~/workspace/dotfiles/sheldon ~/.config/sheldon
+create_link ~/workspace/dotfiles/wezterm ~/.config/wezterm
+create_link ~/workspace/dotfiles/nvim ~/.config/nvim
